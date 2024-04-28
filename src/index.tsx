88681b090f11c4ebff39172213779aaf3750fe4a
Alex <aironer@yandex.ru>
2024-04-28 14:37:39 +0300

    Создал глобальные стили, установил axios и react-router

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import App from "../src/Components/App/App"
+import GlobalStyles from "./Components/GlobalStyles/GlobalStyles"
+import { HashRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 )
